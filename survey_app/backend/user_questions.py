154acac8214f2d7d3c75304c4dcfc14bc9c82a5e
Ned Santiago <nedsantiago@tutanota.com>
2023-11-12 00:41:17 +0800

    created an id system for the storm names

--- survey_app/backend/user_questions.py
+++ survey_app/backend/user_questions.py
@@ -1,8 +1,7 @@
-from csv import DictReader
 from re import sub
 from backend.data_managers import DataRetriever, HtmlContextCreator
 
-def generate_questions(row):
+def generate_id_labels_options(row):
     # list of dictionariies that may replace the options
     list_entries = []
 
@@ -11,16 +10,34 @@ def generate_questions(row):
         # if there is a value in the options
         if value:
             # create the id/name (radio only uses this as id)
-            # by appending a lower-cased, underscored "value" to the "form_name"
-            id = row["form_name"] + "_" + sub(" ","_",value.lower())
+            # by appending a lower-cased, underscored "value" to the "db_name"
+            id = row["db_name"] + "_" + sub(" ","_",value.lower())
             list_entries.append({"id":id, "value":value})
         # else it is assumed not to have any options
         else:
-            id = row["form_name"]
+            id = row["db_name"]
             list_entries.append({"id":id, "value":""})
     
     # create stuff
     row["options"] = list_entries
+
+    return row
+
+def generate_id_labels_repeat_labels(row):
+
+    # list of dictionariies that may replace the storm_name
+    list_entries = []
+
+    # iterate over a comma seperated list in storm_name column
+    for value in row["storm_name"].split(","):
+        # create an id name based on the value
+        id = sub(" ","_",value.lower())
+        # create the dictionary
+        list_entries.append({"id":id, "value":value})
+
+    
+    # create stuff
+    row["storm_name"] = list_entries
 
     return row
 
@@ -34,25 +51,28 @@ def create_questions_context():
 
     questions_data = HtmlContextCreator(
         raw_data=raw_questions_data, 
-        per_row_processor=generate_questions
+        per_row_processor=generate_id_labels_options
         ).get_context()
 
     return questions_data
 
-def create_settings_context():
-    """This method will create a context for the settings (e.g. description_subject: "Storm Name", 
-    list_subject: ["Storm Event 1", "Storm Event 2", "Storm Event 3"])"""
+def create_questions_repeat_labels_context():
+    """This method will create a context for the repeating questiosn (e.g. description_subject: "Storm Name", 
+    list_subject: ["Storm Event 1", "Storm Event 2", "Storm Event 3"]). To explain, sometimes the same question
+    depending on the situation. For example, What is the flood depth? can be asked again depending on the storm
+    in question. This method will list the data to be iterated"""
 
-    # declare the path to the settings.csv file
+    # declare the path to the iterable labels file
+    path_repeat_labels = r"survey_app\backend\static\backend\questions_repeat_labels.csv"
 
-    # open the csv file as utf-8-sig
+    # open using the DataRetriever
+    raw_questions_data = DataRetriever(
+        path_to_database=path_repeat_labels
+        ).get_raw_data()
 
-    # read the file onto a dictreader object
+    questions_data = HtmlContextCreator(
+        raw_data=raw_questions_data, 
+        per_row_processor=generate_id_labels_repeat_labels
+        ).get_context()
 
-    # iterate through the dictreader object
-
-    # create a dictionary with parameters as key and a list of values as value
-    
-    # return the new list_dict
-
-    pass
+    return questions_data
--- survey_app/backend/views.py
+++ survey_app/backend/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from backend.user_questions import create_questions_context
+from backend.user_questions import create_questions_context, create_questions_repeat_labels_context
 
 
 def hello_world(request):
@@ -16,18 +16,18 @@ def survey_about(request):
 def survey_form(request):
     # initialize a list of questions
     questions = create_questions_context()
+    test_sample = questions[12]
+    repeat_labels = create_questions_repeat_labels_context()
+
+    # add the context for the rendering
+    context = {
+            "questions": questions,
+            "test_sample": test_sample,
+            "repeat_labels": repeat_labels
+        }
 
     # if request is POST
     if request.method == 'POST':
-        context = {
-            "questions": questions,
-            "survey_date": request.POST.get('survey_date'),
-            "survey_address": request.POST.get('survey_address'),
-            "respondent_name": request.POST.get('respondent_name'),
-            "respondent_age": request.POST.get('respondent_age'),
-            "respondent_residency_type_resident": request.POST.get('respondent_residency_type_resident'),
-            "respondent_residency_type_employee": request.POST.get('respondent_residency_type_employee')
-        }
         for key in context.keys():
             print(f"{key}: {context[key]}")
         # then it must be receiving data from form submission
@@ -37,9 +37,5 @@ def survey_form(request):
     
     # else it was a simple request
     else:
-        # add the questions as part of the context
-        context = {
-            "questions": questions
-        }
         # render with the context
         return render(request, 'backend/survey_form.html', context)
